5842679c08bd21258b5d4db071a13f4985b4be7a
StephenKiiyuruNganga <40911055+StephenKiiyuruNganga@users.noreply.github.com>
2022-09-02 09:03:19 +0300

    refactor test

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,12 +1,30 @@
 import { expect, it, test } from "vitest"
 import { add } from "./math"
 
-// * it and test are the same
+/* 
+* it and test are the same
+
+* ARRANGE, ACT, ASSERT pattern
+
+  arrange - define testing env and values
+  act - run the actual code/function that should be tested
+  assert - compare the test result to the expected result 
+
+* 
+
+*/
 
 it("should add up all number values in an array", () => {
-  // * 1. your unit
-  const result = add([1, 2, 3])
+  // * 1. arrange
+  const arr = [1, 2, 3]
+  const expected_result = arr.reduce(
+    (prevSum, currentNum) => prevSum + currentNum,
+    0
+  )
 
-  // * 2. the answer
-  expect(result).toBe(6)
+  // * 2. act
+  const test_result = add(arr)
+
+  // * 3. assert
+  expect(test_result).toBe(expected_result)
 })
