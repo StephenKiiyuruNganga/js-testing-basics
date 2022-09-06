06b386df395664203270f01770a91274b4d58c14
StephenKiiyuruNganga <40911055+StephenKiiyuruNganga@users.noreply.github.com>
2022-09-06 07:50:18 +0300

    add new tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -28,3 +28,50 @@ it("should add up all number values in an array", () => {
   // * 3. assert
   expect(test_result).toBe(expected_result)
 })
+
+it("should return NaN if at least one invalid number is provided", () => {
+  const arr = ["stephen", 2, 3]
+
+  const result = add(arr)
+
+  expect(result).toBeNaN()
+})
+
+it("should return a correct sum if an array of numeric string values is provided", () => {
+  const arr = ["1", "2", "3"]
+  const expected_result = arr.reduce(
+    (prevSum, currentNum) => +prevSum + +currentNum,
+    0
+  )
+
+  const result = add(arr)
+
+  expect(result).toBe(expected_result)
+})
+
+it("should return a sum of 0 if an empty array is provided", () => {
+  const arr = []
+
+  const result = add(arr)
+
+  expect(result).toBe(0)
+})
+
+it("should throw an error if no value is passed", () => {
+  const resultHandler = () => {
+    add()
+  }
+
+  expect(resultHandler).toThrow(/is not iterable/)
+})
+
+it("should throw an error if multiple arguments are provided instead of an array", () => {
+  const num1 = 1
+  const num2 = 2
+
+  const resultHandler = () => {
+    add(num1, num2)
+  }
+
+  expect(resultHandler).toThrow(/is not iterable/)
+})
