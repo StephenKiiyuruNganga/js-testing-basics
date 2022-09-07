5f8313bae0660be2e9267769f35570d768174c41
StephenKiiyuruNganga <40911055+StephenKiiyuruNganga@users.noreply.github.com>
2022-09-07 08:41:02 +0300

    add integration test

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,21 +1,43 @@
-import { it, expect } from "vitest"
-import { transformToNumber } from "./numbers"
+import { it, expect, describe } from "vitest"
+import { cleanNumbers, transformToNumber } from "./numbers"
 
-it("should transform a string number to a number of type number", () => {
-  const input = "1"
+describe("transformToNumber()", () => {
+  it("should transform a string number to a number of type number", () => {
+    const input = "1"
 
-  const result = transformToNumber(input)
+    const result = transformToNumber(input)
 
-  expect(result).toBeTypeOf("number")
+    expect(result).toBeTypeOf("number")
+  })
+
+  it("should yield NaN for non-transformable inputs", () => {
+    const input = "stephen"
+    const input2 = {}
+
+    const result = transformToNumber(input)
+    const result2 = transformToNumber(input2)
+
+    expect(result).toBeNaN()
+    expect(result2).toBeNaN()
+  })
 })
 
-it("should yield NaN for non-transformable inputs", () => {
-  const input = "stephen"
-  const input2 = {}
+// * when you test a fn that calls other fns, this is known as an integration test
 
-  const result = transformToNumber(input)
-  const result2 = transformToNumber(input2)
+describe("cleanNumbers()", () => {
+  it("should return an array of numbers when an array of string numbers is provided", () => {
+    const input = ["1", "2"]
 
-  expect(result).toBeNaN()
-  expect(result2).toBeNaN()
+    const result = cleanNumbers(input)
+
+    expect(result[0]).toBeTypeOf("number")
+  })
+
+  it("should throw an error if one of the values from the input is an empty string", () => {
+    const input = ["", "2"]
+
+    const resultHandler = () => cleanNumbers(input)
+
+    expect(resultHandler).toThrow(/empty/)
+  })
 })
